be8e5f34c857238d73800131784dc909ad9b9c7d
root <root@Megahost.1719752907>
2025-03-04 19:50:17 +0700

    Modify to deploy

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
-const PORT = 5000;
+const PORT = process.env.PORT;
 
 const corsEnv = {
 	'development': {
@@ -17,6 +17,7 @@ const corsConfig = corsEnv[process.env.NODE_ENV || 'development'];
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors();
+    console.log(`Lottery app started with port ${PORT}`);
 	await app.listen(PORT);
 }
 bootstrap();
