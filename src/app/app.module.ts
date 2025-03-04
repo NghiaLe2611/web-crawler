bdb18e34eac9aa05c6cd75b60bad78421c583b38
Nghia Le <nghiapro2611@gmail.com>
2025-03-04 20:49:47 +0700

    Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PuppeteerService } from 'src/modules/puppeteer/puppeteer.service';
 import { PuppeteerController } from 'src/modules/puppeteer/puppeteer.controller';
+import { ConfigModule } from '@nestjs/config';
+
+// import { config } from 'dotenv';
+// config();
 
 @Module({
-  imports: [],
-  controllers: [AppController, PuppeteerController],
-  providers: [AppService, PuppeteerService],
+	imports: [
+		ConfigModule.forRoot({
+			envFilePath:
+				process.env.NODE_ENV === 'production'
+					? '.env.production'
+					: '.env',
+			isGlobal: true, // Makes ConfigModule available globally
+		}),
+	],
+	controllers: [AppController, PuppeteerController],
+	providers: [AppService, PuppeteerService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,28 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
+import { ConfigService } from '@nestjs/config';
 
 const PORT = process.env.PORT;
 
 const corsEnv = {
-	'development': {
+	development: {
 		origin: true,
 		credentials: true,
 	},
-	'production': {
-		origin: process.env.DOMAINS ? process.env.DOMAINS.split(",") : '*'
-	}
-}
+	production: {
+		origin: process.env.DOMAINS ? process.env.DOMAINS.split(',') : '*',
+	},
+};
 const corsConfig = corsEnv[process.env.NODE_ENV || 'development'];
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	const configService = app.get(ConfigService);
+	const port = configService.get<number>('PORT') || 5000;
+    const env = configService.get<number>('NODE_ENV') || 'development';
+
 	app.enableCors();
-    console.log(`Lottery app started with port ${PORT}`);
+	console.log(`Lottery app started with port ${port} in ${env}`);
 	await app.listen(PORT);
 }
 bootstrap();
